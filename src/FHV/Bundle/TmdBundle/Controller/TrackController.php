6c8760e8184c8f58f5fb62e40d055a8012954995
Michael Zangerle <michael.zangerle@gmail.com>
2015-04-22 14:47:54 +0200

    added track controller and routing

---
+++ src/FHV/Bundle/TmdBundle/Controller/TrackController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FHV\Bundle\TmdBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+
+/**
+ * Class TrackController
+ * @package FHV\Bundle\TmdBundle\Controller
+ */
+class TrackController extends FOSRestController implements ClassResourceInterface
+{
+    /**
+     * @param $id
+     */
+    public function getAction($id){
+
+    }
+}
